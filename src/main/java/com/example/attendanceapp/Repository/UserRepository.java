51db9a3b75c1aef3189cf4fd1a7948baee45c7aa
unknown <avilasonson@gmail.com>
2024-03-23 09:51:52 +0800

    change from crud repo to jpa repo

--- src/main/java/com/example/attendanceapp/Repository/UserRepository.java
+++ src/main/java/com/example/attendanceapp/Repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.attendanceapp.Repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.attendanceapp.Models.User;
 
-public interface UserRepository extends CrudRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer>{
     
 }
