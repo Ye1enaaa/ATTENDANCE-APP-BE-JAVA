5e4c7ebd27507c6f3783f6cffb76dadce28779c7
unknown <avilasonson@gmail.com>
2024-03-25 08:46:06 +0800

    add findByEmployee option

--- src/main/java/com/example/attendanceapp/Repository/UserRepository.java
+++ src/main/java/com/example/attendanceapp/Repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.attendanceapp.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.attendanceapp.Models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-    
+    Optional<User> findByEmployeeId(Integer employeeId);
 }
