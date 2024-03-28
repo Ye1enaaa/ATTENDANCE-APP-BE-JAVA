acdd42323d6096e54078a9a359efe1e6c71253b2
unknown <avilasonson@gmail.com>
2024-03-28 21:07:06 +0800

    implements serializable

--- src/main/java/com/example/attendanceapp/Models/User.java
+++ src/main/java/com/example/attendanceapp/Models/User.java
@@ -1,23 +1,35 @@
 package com.example.attendanceapp.Models;
 
+import java.io.Serializable;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
 
 @Entity
-public class User {
+@Table
+public class User implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 
     private Integer id;
+
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String address;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private Integer employeeId;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
