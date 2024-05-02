4901514da4e756bc273fa8ec3c873bc1d71275de
unknown <avilasonson@gmail.com>
2024-05-02 15:53:15 +0800

    add jwt secured user api route

--- src/main/java/com/example/attendanceapp/Controllers/UserController.java
+++ src/main/java/com/example/attendanceapp/Controllers/UserController.java
@@ -1,32 +1,55 @@
 package com.example.attendanceapp.Controllers;
 
+//import java.util.Collections;
 import java.util.HashMap;
 import java.util.Optional;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.attendanceapp.Models.User;
+//import com.example.attendanceapp.Providers.JwtTokenProvider;
 import com.example.attendanceapp.Repository.UserRepository;
+//import com.example.attendanceapp.UTILS.JwtUtils;
+import com.example.attendanceapp.Security.JwtIssuer;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+
+//import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
-@RequestMapping(path = "/api")
 public class UserController {
+    @Autowired
+    private JwtIssuer jwtIssuer;
+    
     @Autowired
     private UserRepository userRepository;
 
     PasswordEncoder passwordEncoder;
+
+    
+    //private SecretKey secretKey = JwtUtils.generateSecretKey();
 
     @PostMapping(path = "/add/user")
     public @ResponseBody Object addUser(@RequestBody User userBody) {
@@ -70,17 +93,33 @@ public class UserController {
         return users;
     }
 
-    @GetMapping(path = "/user/{id}")
-    public @ResponseBody Object getMyCredentials(@PathVariable Integer id) {
-        Optional<User> user = userRepository.findById(id);
-        if(!user.isPresent()){
-            HashMap<String, String> responseData = new HashMap<>();
-            responseData.put("msg", "No User was Found");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseData);
-        }else{
-            HashMap<String, Object> responseData = new HashMap<>();
-            responseData.put("data", user);
-            return ResponseEntity.status(HttpStatus.OK).body(responseData);
+   
+    @GetMapping(path="/user")
+    public @ResponseBody Object getMyCredentials(HttpServletRequest request) {
+        String token = resolveToken(request);
+        if (token == null) {
+            System.out.println("No token"); 
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("No token provided");
+        }
+
+        try {
+            DecodedJWT decodedJWT = jwtIssuer.validateAndDecode(token);
+            System.out.println(decodedJWT.getPayload()); 
+            String userId = decodedJWT.getSubject();
+            String email = decodedJWT.getClaim("email").asString();
+            return ResponseEntity.status(HttpStatus.OK).body("User ID: " + userId + ", Email: " + email);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
         }
     }
+    
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); 
+        }
+        return null;
+    }
+   
 }
