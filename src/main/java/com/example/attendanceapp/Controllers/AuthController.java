bfc79dd721a1252ccb601d9deb51fda2e80b8cd3
unknown <avilasonson@gmail.com>
2024-05-03 10:13:20 +0800

    SET ACCESS TOKEN AS HTTP ONLY COOKIES

--- src/main/java/com/example/attendanceapp/Controllers/AuthController.java
+++ src/main/java/com/example/attendanceapp/Controllers/AuthController.java
@@ -1,11 +1,9 @@
 package com.example.attendanceapp.Controllers;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,21 +11,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.attendanceapp.Models.User;
-//import com.example.attendanceapp.Providers.JwtTokenProvider;
 import com.example.attendanceapp.Repository.UserRepository;
 import com.example.attendanceapp.Security.JwtIssuer;
-//import com.example.attendanceapp.UTILS.JwtUtils;
 
-// import io.jsonwebtoken.Jwts;
-// import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
-@SuppressWarnings("deprecation")
+
 @Controller
 public class AuthController {
 
@@ -42,7 +37,7 @@ public class AuthController {
     //private static final long JWT_EXPIRATION_MS = 86400000;
 
     @PostMapping(path = "/auth/login")
-    public @ResponseBody ResponseEntity<?> logIn(@RequestBody User credentials) {
+    public @ResponseBody ResponseEntity<?> logIn(@RequestBody User credentials, HttpServletResponse response) {
         Optional<User> findUsingEmail = userRepository.findByEmail(credentials.getEmail());
         var accessToken = jwtIssuer.issue(1, credentials.getEmail());
         try {
@@ -54,11 +49,14 @@ public class AuthController {
             boolean result = passwordEncoder.matches(credentials.getPassword(), user.getPassword());
 
             if (result) {
-                //String token = generateJwtToken(user.getEmail());
+                Cookie cookie = new Cookie("accessToken", accessToken);
+                cookie.setMaxAge(7 * 24 * 60 * 60);
+                cookie.setPath("/");
+                cookie.setSecure(true);
+                cookie.setHttpOnly(true);
+                response.addCookie(cookie);
                 HashMap<String, Object> responseDatas = new HashMap<>();
                 Map<String, String> responseData = new HashMap<>();
-                //responseData.put("secretKey", secretKey.toString());
-                //responseData.put("token", token);
                 responseData.put("accessToken", accessToken);
                 responseDatas.put("values", responseData);
                 return ResponseEntity.status(HttpStatus.OK).body(responseDatas);
